9a43e0fb39fe28033ecd300629d9833c85db07fe
Nuno Santos <NunoSantos2018@hotmai.com>
2021-11-23 16:28:04 +0000

    Random Commit

---
+++ Meta 2/cmake-build-debug/CMakeFiles/TP_POO.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_POO.dir/TP/comandos.cpp.o"
+  "CMakeFiles/TP_POO.dir/TP/config.cpp.o"
+  "CMakeFiles/TP_POO.dir/TP/ilha.cpp.o"
+  "CMakeFiles/TP_POO.dir/TP/main.cpp.o"
+  "CMakeFiles/TP_POO.dir/TP/trabalhdores.cpp.o"
+  "CMakeFiles/TP_POO.dir/TP/zona.cpp.o"
+  "TP_POO"
+  "TP_POO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_POO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
